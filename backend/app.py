ebb40c178feaaa7ab9996a6a3b52e361e71299e5
Parth Patel <parthpatel@192.168.1.10>
2023-04-13 00:22:23 +0530

    Added backend logic with beautifulsoup with logging

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+from .constants import *
 from flask import Flask, request, jsonify
 import logging
 from bs4 import BeautifulSoup
@@ -10,21 +11,20 @@ app = Flask(__name__)
 
 @app.route('/amazon_search', methods=['GET'])
 def amazon_search():
-    keyword = request.args.get('keyword')
-    url = f'https://www.amazon.in/s?k={keyword}'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',}
+    keyword = request.args.get(KEYWORD)
+    url = AMAZON_URL + keyword
+    headers = AMAZON_API_HEADER
     response = requests.post(url, headers=headers)
     app.logger.info(f'Response: {response.status_code}')
     soup = BeautifulSoup(response.text, 'html.parser')
     products = []
     for item in soup.select('.s-result-item'):
         try:
-            print(item)
             product = {
-                'title': item.select_one('.a-size-medium.a-color-base.a-text-normal').text.strip(),
-                'price': item.select_one('.a-price .a-offscreen').text.strip(),
-                'rating': item.select_one('.a-icon-star-small .a-icon-alt').text.strip(),
-                'url': item.select_one('.a-link-normal')['href']
+                TITLE: item.select_one('.a-size-medium.a-color-base.a-text-normal').text.strip(),
+                PRICE: item.select_one('.a-price .a-offscreen').text.strip(),
+                RATING: item.select_one('.a-icon-star-small .a-icon-alt').text.strip(),
+                URL: item.select_one('.a-link-normal')['href']
             }
             products.append(product)
         except Exception as e:
